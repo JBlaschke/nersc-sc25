9b7009e73ff735d9c683f2e93812695185b361ea
Johannes Blaschke <jpblaschke@lbl.gov>
2025-11-04 17:33:33 -0800

    sudo has different path

--- post/configure-slurm.sh
+++ post/configure-slurm.sh
@@ -2,10 +2,10 @@
 set -euo pipefail
 
 # On the controller node or login node with sacctmgr access
-sudo sacctmgr add account workshop Description="SC25 Workshop Account" -i
+sudo /usr/local/bin/sacctmgr add account workshop Description="SC25 Workshop Account" -i
 
 for i in {1..10}; do
-    sudo sacctmgr add user user${i} account=workshop -i
+    sudo /usr/local/bin/sacctmgr add user user${i} account=workshop -i
 done
 
 # Verify
