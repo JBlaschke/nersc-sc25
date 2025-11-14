6d58457ab166e2e1de244521eeb98398c0037621
Johannes Blaschke <jpblaschke@lbl.gov>
2025-11-13 21:57:42 -0800

    typo

--- post/configure-slurm.sh
+++ post/configure-slurm.sh
@@ -4,7 +4,7 @@ set -euo pipefail
 # On the controller node or login node with sacctmgr access
 sudo /usr/local/bin/sacctmgr add account workshop Description="SC25 Workshop Account" -i
 
-for i in {1..10}; do
+for i in {1..100}; do
     sudo /usr/local/bin/sacctmgr add user user${i} account=workshop -i
 done
 
